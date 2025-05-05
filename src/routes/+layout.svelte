64f3da3ba2ec992ab65693ef56029c3a86d59102
Aerin Gamaroff <aerin.gamaroff@gmail.com>
2025-05-05 12:59:57 +0200

    login page and button component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,17 +5,29 @@
 	import 'modern-normalize/modern-normalize.css';
 
 	import type { LayoutData } from './$types';
-	import LogoutButton from '$components/LogoutButton.svelte';
 
 	export let data: LayoutData;
 
 	$: user = data.user;
 </script>
 
-{#if user}
-	<p>Hello, {user.display_name}</p>
+<div id="main">
+	<div id="content">
+		<main id="main-content">
+			<slot />
+		</main>
+	</div>
+</div>
 
-	<LogoutButton />
-{/if}
-
-<slot />
+<style lang="scss">
+	#main {
+		#content {
+			main#main-content {
+				padding: 30px 15px 60px;
+				@include breakpoint.up('md') {
+					padding: 30px 30px 60px;
+				}
+			}
+		}
+	}
+</style>
